36575eeb547e6f0262ca9c2d21d9162e3049d918
Miroslav Petrik <miroslavpetrik@outlook.com>
2022-11-06 10:02:28 +0100

    cleanup

--- edgedb/src/queries/countNotificationsQuery.ts
+++ edgedb/src/queries/countNotificationsQuery.ts
@@ -12,10 +12,3 @@ export const countNotificationsQuery = e.with(
   [currentUserTasks],
   e.select({ count: e.count(currentUserTasks) })
 )
-
-// with N := (select Task filter .hasNotification and global currentUser in .assignees)
-// select {
-//   matches := N {status},
-//   total := count(N),
-//   totalTasks := count(Task),
-// };
